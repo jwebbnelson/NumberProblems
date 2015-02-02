11945215dc89020ef3e26fa56b4d56c8d6981a54
Jordan Nelson <nelsonjordan20@yahoo.com>
2015-02-02 01:22:09 -0700

    Completed NumberProblems
    PreCourse Project 5

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -8,6 +8,10 @@
 
 #import "AppDelegate.h"
 
+static int multiple = 8;
+static int divisor = 6;
+
+
 @interface AppDelegate ()
 
 @end
@@ -15,12 +19,37 @@
 @implementation AppDelegate
             
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
-    // Insert code here...
-    
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOption
+{
+    [self iterateCount:4];
+    [self multiplied:3];
     
     return YES;
+}
+
+-(void)iterateCount:(float)number{
+    
+    float myInt = [self multiplied:number];
+    NSLog(@"Mulitiplied %f by %d to get %f", number, multiple, myInt);
+    
+    float myIntDivided = [self divided:myInt];
+    NSLog(@"Divided %f by %d to get %f", myInt, divisor, myIntDivided);
+    
+    if (number == 0) {
+        return;
+    }
+    else{
+        NSLog(@"Iterate %f", number);
+        number--;
+        [self iterateCount:number];
+    }
+}
+-(float)multiplied:(float)number{
+    return number * multiple;
+}
+
+-(float)divided:(float)number{
+    return number/divisor;
 }
 
 @end
